fbbe7e4f4d7a214e671b0aece8f39646abc485fd
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-03-11 14:48:50 +0000

    Added an easier way to test the functionality with a server which is started and stoped

--- schmokin_test.go
+++ schmokin_test.go
@@ -2,34 +2,57 @@ package main
 
 import (
 	"testing"
+	"net/http"
+	"context"
+	"log"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Status_Equals(t *testing.T) {
-	var httpClient = CreateCurlHttpClient()
-	var app = CreateSchmokinApp(httpClient)
-	var args = []string{
-		"http://localhost:40000/pretty",
-		"--status",
-		"--eq",
-		"200",
-	}
+func Test_Schmokin(t *testing.T){
 
-	var result = app.schmoke(args)
-	assert.True(t, result.success)
-}
+	
+	m := http.NewServeMux()
+	s := http.Server{Addr: ":40000", Handler: m}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer func(){
+		cancel()
+		s.Shutdown(ctx)
+	}()
+	m.HandleFunc("/pretty", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
 
-func Test_Status_NotEquals(t *testing.T) {
-	var httpClient = CreateCurlHttpClient()
-	var app = CreateSchmokinApp(httpClient)
-	var args = []string{
-		"http://localhost:40000/pretty",
-		"--status",
-		"--ne",
-		"201",
-	}
+	t.Run("Test_Status_Equals", func (t *testing.T) {
+		var httpClient = CreateCurlHttpClient()
+		var app = CreateSchmokinApp(httpClient)
+		var args = []string{
+			"http://localhost:40000/pretty",
+			"--status",
+			"--eq",
+			"200",
+		}
 
-	var result = app.schmoke(args)
-	assert.True(t, result.success)
+		var result = app.schmoke(args)
+		assert.True(t, result.success)
+	})
+
+	t.Run("Test_Status_NotEquals", func (t *testing.T) {
+		var httpClient = CreateCurlHttpClient()
+		var app = CreateSchmokinApp(httpClient)
+		var args = []string{
+			"http://localhost:40000/pretty",
+			"--status",
+			"--ne",
+			"201",
+		}
+
+		var result = app.schmoke(args)
+		assert.True(t, result.success)
+	})
 }
